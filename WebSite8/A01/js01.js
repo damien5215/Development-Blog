b07beb29428648dd97b447d4a1af5ccaebb87f8b
damien5215 <37832934+damien5215@users.noreply.github.com>
2019-10-18 22:27:06 +0100

    (18/10/2019)
    Trying to use just JavaScript (vanilla).

---
+++ WebSite8/A01/js01.js
@@ -0,0 +1,27 @@
+﻿
+const dataURL = "http://www.text.damienasp.com/jsonData.json";
+const btnSelect = document.getElementsByTagName("input");
+document.querySelector('#img1').src = "http://www.text.damienasp.com/images2/D002b.png";     // set default image
+
+document.getElementById("btn0").style.backgroundColor = "red";          // set default to red.
+
+
+for (let i = 0; i < btnSelect.length; i += 1) {
+    btnSelect[i].addEventListener("click", function (e) {
+        console.log("You clicked btn" + i + "! " + event.target.value);    // event.target, this.className
+
+        for (let i = 0; i < btnSelect.length; i += 1) {
+            document.querySelector('#btn' + i).style.backgroundColor = "black";
+        }
+        event.target.style.backgroundColor = "red";     // set color of clicked button
+        //$("#img1").fadeOut(700);                      // TEST DELETE LATOR, it works!!!
+    })
+}
+
+
+//async function getData(dataURL){
+//    const response1 = await fetch(dataURL);
+//    const getDataJSON = await response1.json();
+//    return getDataJSON;
+//}
+
--- WebSite8/ExternalJquery.js
+++ WebSite8/ExternalJquery.js
@@ -1,9 +1,6 @@
 ﻿$(document).ready(function () {
-
-    var x1 = 700;                                                  // variable sets fadeIn speed
   
     $(".button").click(function () {                               // target by class (button)
-        $("#img1").fadeOut(50);                                    // hide image,      $("#img1").css("display", "none");
 
         $(".button").css("background-color", "black");             // target by class (button) and set all buttons to "black"
         var test1 = $(this).attr('id');                            // get id of .button clicked ("butt2")
@@ -14,20 +11,23 @@
         getData.open('GET', 'http://www.text.damienasp.com/jsonData.json');    // JSON file
         getData.onload = function () {
             var data1 = JSON.parse(getData.responseText);
-            var id = data1[11].buttonArray.indexOf(test1)           // variable holds index-value of clicked-button (2 = "butt2")
-            var textJSON = data1[12].textArray2[id].replace(/xxx/g, "\n");    // Contains formatted text 
+            var id = data1[11].buttonArray.indexOf(test1)                    // holds index-value of clicked-button (2 = "butt2")
+            var textJSON = data1[12].textArray2[id].replace(/xxx/g, "\n");   // contains formatted text 
+            $("#img2").attr("src", data1[id].imgString2);                    // set img2 (hidden by CSS), stores in cache early
 
             console.log(id);
             console.log(data1[id].buttonNum2);
             console.log(data1[id].imgString2);
-            //console.log(data1[12].textArray2[id]);                   // Coded string (no formatting)  
-            //console.log(textJSON);                                   // Uncoded string (contains formatting)
+            //console.log(data1[12].textArray2[id]);                   // coded string (no formatting)  
+            //console.log(textJSON);                                   // uncoded string (contains formatting)
         
-        $("#text1").text(textJSON);                                 // Load text (contains formatting) 
-        $("#img1").attr("src", data1[id].imgString2)                // index of button, selects the desired image
-        $("#img1").load(data1[id].imgString2);                      // load and set image
-        $("#img1").fadeIn(x1);                                      // fade-in image
-        };
+            $("#img1").fadeOut(500, function(){
+                $("#text1").text(textJSON);                                 // load text (contains formatting) 
+                $("#img1").attr("src", data1[id].imgString2);               // index of button, selects the desired image
+                $("#img1").fadeIn(700);
+                console.log("New text and image set.");
+            });               
+        };                           
         getData.send();
-    });
+    });           
 });
