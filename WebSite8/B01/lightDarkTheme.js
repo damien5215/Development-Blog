c21b903fe5c45c5f0714f182cc3d5efc4e7170ea
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-08-06 22:30:35 +0100

    Added Dark Theme Toggle

---
+++ WebSite8/B01/lightDarkTheme.js
@@ -0,0 +1,16 @@
+﻿
+
+var darkMode = false;
+
+function lightDark() {
+
+    if (darkMode) {
+        document.querySelector("link[href='StyleSheetDark.css']").href = "StyleSheet1.css";
+        darkMode = false;
+    }
+    else {
+        document.querySelector("link[href='StyleSheet1.css']").href = "StyleSheetDark.css";
+        darkMode = true;
+    }
+
+};
